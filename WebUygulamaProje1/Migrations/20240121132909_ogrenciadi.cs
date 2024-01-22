364f954c78df48c485006ae8f25134679a8575e6
m0rh <memiswork@gmail.com>
2024-01-22 11:21:43 +0300

    Orhan Kütüphanesi
    admin girişi için
    eposta : admin1@gmail.com
    şifre: Memoli_35

---
+++ WebUygulamaProje1/Migrations/20240121132909_ogrenciadi.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebUygulamaProje1.Migrations
+{
+    /// <inheritdoc />
+    public partial class ogrenciadi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OgrenciAdi",
+                table: "Kiralamalar",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OgrenciAdi",
+                table: "Kiralamalar");
+        }
+    }
+}
---
+++ WebUygulamaProje1/Models/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebUygulamaProje1.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+        [Required]
+        public string? OgrenciIsımSoyisim { get; set; } 
+
+        [Required]
+        public int Ogrencino { get; set; }
+
+        public string? Adres { get; set; }
+        public string? Fakulte { get; set; }
+        public string? Bolum { get; set; }
+    }
+}
